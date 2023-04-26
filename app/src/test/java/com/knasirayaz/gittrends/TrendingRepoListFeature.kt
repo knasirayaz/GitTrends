56446b28388484f6b2199a88c443a60cbaa8298b
Nasir Ayaz <knasirayaz@gmail.com>
2023-04-26 14:15:14 +0500

    added-mockito-dependancy

---
+++ app/src/test/java/com/knasirayaz/gittrends/TrendingRepoListFeature.kt
@@ -0,0 +1,15 @@
+package com.knasirayaz.gittrends
+
+import org.junit.Test
+
+class TrendingRepoListFeature {
+
+
+    //Acceptance Test
+    @Test
+    fun `fetch trending repo list`(){
+        //we will have observer to observe when state changes.
+        // - Need to mock observer and verify its state is changing.
+
+    }
+}
