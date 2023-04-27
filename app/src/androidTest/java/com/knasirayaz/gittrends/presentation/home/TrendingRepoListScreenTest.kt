19f95b0b4beeee0ba8fee0cf8fcf73204b78597f
Nasir Ayaz <knasirayaz@gmail.com>
2023-04-27 14:56:54 +0500

    fix-hilt-repo-issues-and-refactored-ui-tests

--- app/src/androidTest/java/com/knasirayaz/gittrends/presentation/home/TrendingRepoListScreenTest.kt
+++ app/src/androidTest/java/com/knasirayaz/gittrends/presentation/home/TrendingRepoListScreenTest.kt
@@ -2,9 +2,7 @@ package com.knasirayaz.gittrends.presentation.home
 
 import androidx.activity.compose.setContent
 import androidx.compose.ui.test.assert
-import androidx.compose.ui.test.assertAll
 import androidx.compose.ui.test.assertCountEquals
-import androidx.compose.ui.test.assertTextContains
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onChildAt
@@ -33,87 +31,22 @@ class TrendingRepoListScreenTest {
 
     @Before
     fun setUp() {
-        composeTestRule.activity.setContent {
-            TrendingRepoListScreen(
-                getTrendingListItems()
-            )
-        }
         context = composeTestRule.activity
     }
 
-    //Sample data to test functionality.
-    private fun getTrendingListItems(): ArrayList<TrendingListItem> {
-        val mTrendingListItems: ArrayList<TrendingListItem> = ArrayList()
-
-        mTrendingListItems.add(
-           TrendingListItem(
-               owner = TrendingListItem.Owner(userProfilePicture = "profilePicture",
-    userName = "TestName-1"),
+    @Test
+    fun verify_all_screen_items_are_present() {
+        val mCurrentTrendingListItem =
+            TrendingListItem(
+                owner = TrendingListItem.Owner(
+                    userProfilePicture = "profilePicture",
+                    userName = "TestName-1"
+                ),
                 repoName = "Kotlin-DSL",
                 repoDesc = "The Kotlin DSL Plugin provides a convenient way to develop Kotlin-based projects that contribute build logic",
                 repoLanguage = "Kotlin",
                 starsCount = "5000"
             )
-        )
-
-        mTrendingListItems.add(
-           TrendingListItem(
-               owner = TrendingListItem.Owner(userProfilePicture = "profilePicture",
-    userName = "TestName-2"),
-                repoName = "Kotlin-DSL",
-                repoDesc = null,
-                repoLanguage = "Kotlin",
-                starsCount = "5000"
-            )
-        )
-
-        mTrendingListItems.add(
-           TrendingListItem(
-               owner = TrendingListItem.Owner(userProfilePicture = "profilePicture",
-    userName = "TestName-3"),
-                repoName = "Kotlin-DSL",
-                repoDesc = "The Kotlin DSL Plugin provides a convenient way to develop Kotlin-based projects that contribute build logic",
-                repoLanguage = null,
-                starsCount = "5000"
-            )
-        )
-
-        mTrendingListItems.add(
-           TrendingListItem(
-               owner = TrendingListItem.Owner(userProfilePicture = "profilePicture",
-    userName = "TestName-4"),
-                repoName = "Kotlin-DSL",
-                repoDesc = "The Kotlin DSL Plugin provides a convenient way to develop Kotlin-based projects that contribute build logic",
-                repoLanguage = "Kotlin",
-                starsCount = null
-            )
-        )
-        return mTrendingListItems
-    }
-
-    @Test
-    fun app_bar_is_present() {
-
-        composeTestRule.onNodeWithTag(context.getString(string.tt_app_bar)).assertExists()
-        composeTestRule.onNodeWithContentDescription(context.getString(string.tt_menu_button))
-            .assertExists()
-    }
-
-    @Test
-    fun trending_list_is_present() {
-        composeTestRule.activity.setContent {
-            TrendingRepoListScreen(
-                getTrendingListItems()
-            )
-        }
-        composeTestRule
-            .onNodeWithTag(context.getString(string.tt_trending_list))
-            .assertExists()
-    }
-
-    @Test
-    fun trending_list_items_are_present() {
-        val mCurrentTrendingListItem = getTrendingListItems().first()
 
         composeTestRule.activity.setContent {
             TrendingRepoListScreen(
@@ -121,6 +54,14 @@ class TrendingRepoListScreenTest {
             )
         }
 
+
+        composeTestRule.onNodeWithTag(context.getString(string.tt_app_bar)).assertExists()
+        composeTestRule.onNodeWithContentDescription(context.getString(string.tt_menu_button))
+            .assertExists()
+
+        composeTestRule
+            .onNodeWithTag(context.getString(string.tt_trending_list))
+            .assertExists()
 
         composeTestRule
             .onNodeWithTag(context.getString(string.tt_list_item))
@@ -179,43 +120,43 @@ class TrendingRepoListScreenTest {
     fun should_show_list_when_list_is_not_empty() {
         composeTestRule.activity.setContent {
             TrendingRepoListScreen(
-                getTrendingListItems()
+                listOf(
+                    TrendingListItem(
+                        owner = TrendingListItem.Owner(userProfilePicture = "profilePicture",
+                            userName = "TestName-1"),
+                        repoName = "Kotlin-DSL",
+                        repoDesc = "The Kotlin DSL Plugin provides a convenient way to develop Kotlin-based projects that contribute build logic",
+                        repoLanguage = "Kotlin",
+                        starsCount = "5000"
+                    )
+                )
             )
         }
 
         composeTestRule
             .onNodeWithTag(context.getString(string.tt_trending_list))
             .onChildren()
-            .assertCountEquals(getTrendingListItems().size)
+            .assertCountEquals(1)
 
     }
 
     @Test
-    fun verify_first_and_last_items_are_available() {
-        composeTestRule.apply {
-            onNodeWithTag(context.getString(string.tt_trending_list))
-                .onChildren()
-                .onFirst()
-                .onChildAt(1)
-                .assert(hasText(getTrendingListItems().first().owner.userName))
-        }
-
-        composeTestRule.apply {
-            onNodeWithTag(context.getString(string.tt_trending_list))
-                .onChildren()
-                .onLast()
-                .onChildAt(1)
-                .assert(hasText(getTrendingListItems().last().owner.userName))
-        }
-
-    }
-
-    @Test
-    fun should_hide_desc_when_desc_is_null(){
+    fun should_hide_desc_when_desc_is_null() {
         composeTestRule.activity.setContent {
             TrendingRepoListScreen(
                 //On Trending List at Index 1, Description is null on sample data.
-                listOf(getTrendingListItems()[1])
+                listOf(
+                    TrendingListItem(
+                        owner = TrendingListItem.Owner(
+                            userProfilePicture = "profilePicture",
+                            userName = "TestName-2"
+                        ),
+                        repoName = "Kotlin-DSL",
+                        repoDesc = null,
+                        repoLanguage = "Kotlin",
+                        starsCount = "5000"
+                    )
+                )
             )
         }
 
@@ -226,12 +167,21 @@ class TrendingRepoListScreenTest {
     }
 
     @Test
-    fun should_hide_language_icon_and_text_when_it_is_null_or_empty(){
+    fun should_hide_language_icon_and_text_when_it_is_null_or_empty() {
         composeTestRule.activity.setContent {
-
-            //On Trending List at Index 2, Language is null on sample data.
             TrendingRepoListScreen(
-                listOf(getTrendingListItems()[2])
+                listOf(
+                    TrendingListItem(
+                        owner = TrendingListItem.Owner(
+                            userProfilePicture = "profilePicture",
+                            userName = "TestName-3"
+                        ),
+                        repoName = "Kotlin-DSL",
+                        repoDesc = "The Kotlin DSL Plugin provides a convenient way to develop Kotlin-based projects that contribute build logic",
+                        repoLanguage = null,
+                        starsCount = "5000"
+                    )
+                )
             )
         }
 
@@ -244,10 +194,19 @@ class TrendingRepoListScreenTest {
     @Test
     fun should_hide_stars_icon_and_text_when_it_is_null_or_empty() {
         composeTestRule.activity.setContent {
-
-            //On Trending List at Index 3, 'Star Count' is null on sample data provided above for test cases.
             TrendingRepoListScreen(
-                listOf(getTrendingListItems()[3])
+                listOf(
+                    TrendingListItem(
+                        owner = TrendingListItem.Owner(
+                            userProfilePicture = "profilePicture",
+                            userName = "TestName-4"
+                        ),
+                        repoName = "Kotlin-DSL",
+                        repoDesc = "The Kotlin DSL Plugin provides a convenient way to develop Kotlin-based projects that contribute build logic",
+                        repoLanguage = "Kotlin",
+                        starsCount = null
+                    )
+                )
             )
         }
 
